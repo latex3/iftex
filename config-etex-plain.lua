b7e61df63bba8d6aa14aa3802d26418dc88aebdc
David Carlisle <d.p.carlisle@gmail.com>
2019-10-26 13:58:58 +0100

    more l3build check config

---
+++ config-etex-plain.lua
@@ -0,0 +1,5 @@
+testfiledir  = "testfiles-etex-plain"
+checkengines= {"pdftex","luatex","xetex","eptex","euptex"}
+checkformat =  "tex"
+checkruns = 1
+
--- config-plain.lua
+++ config-plain.lua
@@ -1,5 +1,5 @@
 testfiledir  = "testfiles-plain"
-checkengines= {"pdftex","luatex","xetex","ptex","uptex"}
-checkformat =  "plain"
+checkengines= {"pdftex","luatex","xetex","ptex","eptex","uptex","euptex","aleph"}
+checkformat =  "tex"
 checkruns = 1
 
